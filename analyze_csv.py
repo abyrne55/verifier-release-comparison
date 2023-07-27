2789eac09de811cbfe90041233ad20f1cbbbab9e
Anthony Byrne <abyrne@redhat.com>
2023-07-27 12:06:54 -0400

    Better input validation

--- analyze_csv.py
+++ analyze_csv.py
@@ -4,6 +4,7 @@ import re
 import sys
 import urllib
 from datetime import datetime
+from enum import Enum
 from typing import Optional
 
 
@@ -28,6 +29,35 @@ def is_valid_url(url):
     return url is not None and regex.search(url)
 
 
+def is_nully(s):
+    """
+    Returns True if s is None, an empty or whitespace-filled string, or some variation of "NULL"
+    """
+    if s is None:
+        return True
+    s_strip = s.lower().strip()
+    return s_strip == "" or s_strip == "null"
+
+
+class OCMState(Enum):
+    """
+    States in which an OCMcluster could be, according to 
+    https://gitlab.cee.redhat.com/service/uhc-clusters-service/-/blob/master/pkg/models/clusters.go
+    """
+
+    VALIDATING = "validating"
+    WAITING = "waiting"
+    PENDING = "pending"
+    INSTALLING = "installing"
+    READY = "ready"
+    ERROR = "error"
+    UNINSTALLING = "uninstalling"
+    UNKNOWN = "unknown"
+    POWERING_DOWN = "powering_down"
+    RESUMING = "resuming"
+    HIBERNATING = "hibernating"
+
+
 class ClusterVerifierRecord:
     """Represents a single row in the CSV"""
 
@@ -36,7 +66,7 @@ class ClusterVerifierRecord:
         timestamp: datetime,
         cid: str,
         cname: Optional[str],
-        ocm_state: Optional[str],
+        ocm_state: Optional[OCMState],
         ocm_inflight_states: Optional[list[str]],
         found_verifier_s3_logs: Optional[bool],
         found_all_tests_passed: Optional[bool],
@@ -53,6 +83,12 @@ class ClusterVerifierRecord:
         self.found_egress_failures = found_egress_failures
         self.log_download_url = log_download_url
 
+    def is_transient(self):
+        """
+        Returns True if cluster is in a "non-steady state" (e.g., installing, validating)
+        """
+        return self.ocm_state not in [OCMState.READY, OCMState.ERROR, OCMState.UNINSTALLING, OCMState.HIBERNATING]
+
     def __gt__(self, other):
         return self.timestamp > other.timestamp
 
@@ -62,21 +98,35 @@ class ClusterVerifierRecord:
     @classmethod
     def from_dict(cls, in_dict: dict[str, str]):
         """Create an instance of this class from a dictionary produced by csv.DictReader"""
+        
+        # Mandatory Fields
         timestamp = datetime.fromisoformat(in_dict["timestamp"].replace("Z", "+00:00"))
-        cid = in_dict["cid"]
-        cname = in_dict["cname"]
-        ocm_state = in_dict["ocm_state"]
-        found_verifier_s3_logs = csv_bool_to_bool(in_dict["found_verifier_s3_logs"])
-        found_all_tests_passed = csv_bool_to_bool(in_dict["found_all_tests_passed"])
-        found_egress_failures = csv_bool_to_bool(in_dict["found_egress_failures"])
+        cid = in_dict["cid"].strip()
+        if is_nully(cid):
+            raise ValueError("Cannot create ClusterVerifierRecord without cluster ID (cid)")
+        
+        # Optional Fields
+        try:
+            cname = None if is_nully(in_dict["cname"]) else in_dict["cname"].strip()
+            found_verifier_s3_logs = csv_bool_to_bool(in_dict["found_verifier_s3_logs"])
+            found_all_tests_passed = csv_bool_to_bool(in_dict["found_all_tests_passed"])
+            found_egress_failures = csv_bool_to_bool(in_dict["found_egress_failures"])
+            ocm_state_str = in_dict["ocm_state"].lower().strip()
+            ocm_inflight_states_str = in_dict["ocm_inflight_states"].strip()
+        except AttributeError as exc:
+            # .strip()/.lower() will raise AttributeError for non-str types, but we consider 
+            # this a ValueError
+            raise ValueError("Non-str-typed keys passed to from_dict()") from exc
+
+        # Finish processing "strictly typed" fields (these will raise their own exceptions)
+        ocm_state = None if is_nully(ocm_state_str) else OCMState(ocm_state_str)
+        ocm_inflight_states = (
+            None if is_nully(ocm_inflight_states_str) else json.loads(in_dict["ocm_inflight_states"])
+        )
         log_download_url = (
             in_dict["log_download_url"] if is_valid_url(in_dict["log_download_url"]) else None
         )
-        try:
-            ocm_inflight_states = json.loads(in_dict["ocm_inflight_states"])
-        except json.decoder.JSONDecodeError:
-            ocm_inflight_states = None
-
+        
         return cls(
             timestamp,
             cid,
