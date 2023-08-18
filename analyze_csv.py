ac907d8b0c9bf53d5e3a264e8f344c54ba82d664
Anthony Byrne <abyrne@redhat.com>
2023-08-18 16:53:20 -0400

    Implemented --(no-)hcp

--- analyze_csv.py
+++ analyze_csv.py
@@ -1,26 +1,27 @@
 """Analyze a CSV produced by the verifier_log_cronjob.sh and print the results"""
 import argparse
 import csv
-import sys
+from multiprocessing import Manager
 
 from models import ClusterVerifierRecord, Outcome
 
+# Parse command line arguments
 arg_parser = argparse.ArgumentParser(
     description="Analyze CSVs produced by verifier_log_cronjob.sh and print the results"
 )
 arg_parser.add_argument(
     "csv_file",
-    type=argparse.FileType(encoding="utf-8"),
-    nargs=1,
+    type=argparse.FileType(),
     help="path to the CSV file under analysis",
 )
 arg_parser.add_argument(
     "--hcp",
     action=argparse.BooleanOptionalAction,
     help=(
-        "analyze ONLY data generated from HyperShift/HCP HostedClusters. "
-        "Conversely, --no-hcp excludes all HostedCluster data. Set "
-        "neither of these to analyze all data"
+        "analyze ONLY data generated from HyperShift/HCP HostedClusters. Conversely, "
+        "--no-hcp excludes all HostedCluster data. Set neither of these to analyze "
+        "all data. NOTE: setting either flag will cause an extra HTTP request per"
+        "cluster ID, likely slowing processing considerably"
     ),
 )
 arg_parser.add_argument(
@@ -35,17 +36,25 @@ arg_parser.add_argument(
     type=str,
     help="ignore data collected after ISO8601_DATETIME",
 )
+args = arg_parser.parse_args()
 
+# Read CSV and create a ClusterVerifierRecord (CVR) from each row
 cvrs = {}
-with open(sys.argv[1], newline="", encoding="utf-8") as f:
-    reader = csv.DictReader(f)
+with Manager() as manager:
+    # Use a "dict manager" for our HCP cache (futureproofing against multiprocessing)
+    hcp_cache = manager.dict()
+    # with open(args.csv_file, newline="", encoding="utf-8") as f:
+    reader = csv.DictReader(args.csv_file)
     for row in reader:
         cvr = ClusterVerifierRecord.from_dict(row)
-        try:
-            cvrs[cvr.cid] += cvr
-        except KeyError:
-            # First time we're seeing a CVR for this cluster ID; store it
-            cvrs[cvr.cid] = cvr
+        # Filter out HCPs according to presence of --(no-)hcp flag
+        if args.hcp is None or args.hcp == cvr.is_hostedcluster(cache=hcp_cache):
+            try:
+                cvrs[cvr.cid] += cvr
+            except KeyError:
+                # First time we're seeing a CVR for this cluster ID; store it
+                cvrs[cvr.cid] = cvr
+args.csv_file.close()
 
 
 print(f"Total deduplicated records: {len(cvrs)}")
